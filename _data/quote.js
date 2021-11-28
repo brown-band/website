89019617e67834a3f0780eeef5baa2885b8aa7f2
Jed Fox <git@jedfox.com>
2021-11-27 23:05:38 -0500

    quote, nav tweaks

---
+++ _data/quote.js
@@ -0,0 +1,114 @@
+module.exports = () => ({
+  all: quotes,
+  random: () => quotes[Math.floor(Math.random() * quotes.length)],
+});
+
+const quotes = [
+  //Fall 2003
+  "an organization with more crust than an empty Ronzio’s box",
+  "an organization whose a-rival has been a-foretold",
+  "an organization that's got your Magic Johnson right here",
+  "What rhymes with penguin?",
+  "So THIS is what it sounds like when doves cry!",
+  "No, you are.",
+  "Globetrotter, I hardly know her!",
+
+  //Spring 2004
+  "an organization that invites you to see Alice run for 4 hours straight",
+  "Roses are red, so are some ants, I bet you can’t guess what I’ve got in my pants",
+
+  //Fall 2004
+  "An organization that's always in the upstate",
+  "An organization with the most pipes in the Ivy League",
+  "Organ Grinder? I hardly know 'er!",
+  "An organization that's different from all other organizations",
+  "Kosher? I hardly know 'er!",
+  "An organization that comes in Light, Regular, Heavy, Super, and Cool Ranch",
+  "You can't gargle sand",
+  "An organization that didn't quite make it to Foxwoods",
+  "Meet me at the Wakefield Rotary Club?",
+  "An organization that doesn't need any wood",
+  "Flip-flopper? I hardly know 'er!",
+  "An organization that's rising up to the challenge of our rival",
+  "Band! Band band band! Band band band! Band band baaaaaaand!",
+  "An organization that prefers not to swallow what it's chewing",
+  "That'll stay in your stomach for 7 years!",
+  "An organization that knows it might be wrong",
+  "Who's your daddy?",
+  "An organization that put its Big 10 in the Happy Valley",
+  "We've got your Beaver Stadium right here!",
+  "An organization that's still taking candy from strangers",
+  "Twizzler? I hardly know 'er!",
+  "An organization that didn't know what to do so it just did a hoe-down",
+  "Why don't you blame it on the rain?",
+  "An organization that's no longer part of a reality-based community",
+  "Blitzer? I hardly know 'er!",
+  "An organization that interrupts this program",
+  "Ennnhhh!",
+  "An organization that's presenting",
+  "Baby got baaaaack!!!",
+  "And organization that's more upper class than a frilly cravat",
+  "Four more years! Four more years!",
+  "An organization that invites you to stuff its body cavity",
+
+  //Spring 2005
+  "An organization that&#8217;s 86% effective when used correctly",
+  "Does my tongue taste funny to you?",
+
+  //Fall 2005
+  "An Organization that steals the Harvard drum as often as it can",
+  "An Organization that vote George Clinton in &#8217;94",
+  "An Organization that knows how to dampen your campus EEEE",
+  "For a good &#8217;ol 5 cents you can smoke my cigar",
+  "An Organization whose Bari Sax section&#8217;s BAC is higher than the average URI student&#8217;s GPA",
+  "NO! IN UR EYE",
+  "An organization that pardoned Galileo in 1969",
+  "Shana Tovah? I hardly know &#8217;ah",
+  "An Organization that sang the hit song 'What if God was Pasta Sauce",
+  "Ramen AND Women",
+
+  //Spring 2006
+  "An Organization that knows the little 'ding' makes all the difference",
+  "Ice Ice Bandie",
+
+  //Fall 2006
+  "an organization that's been on both sides of the benedict",
+  "an Organization that always runs trots and skis!",
+  "an organization that will never stop coming early",
+  "an Organization that not Ruthless",
+  "an Organization that needs a Kleenex every time",
+  "an organization can't wait for the premiere of Doom 2: I still know what you doomed last summer",
+  "an organization that doesn't need grade inflation because it really is just getting smarter",
+  "an Organization that's been giving instruction in any area that counts since '69",
+  "an Organization that puts the 'homey' back in Homecoming",
+  "Holy Water Batman",
+  "why is there red in my Brown!?",
+  "Roshashanna, Roshashanna, Oy Vey Vey, Lech Heim!",
+  "Ruth gets to stay, You get a Chinese vegetable!",
+  "Hurras today, gone tomorrow?",
+  "Keep it Gesund-tight!",
+  "the Bone Saw is Ready!",
+  "000010011011011101110111101100101...2",
+  "...Hey! I What are you doing?!! I didn't even say it yet!",
+  "Duck Duck Goose Goose Goose JaigerM *hurling sound* ",
+
+  //Spring 2007
+  "an organization that was recently voted best scramble band in America in the most recent Quinnipiac poll",
+  "an organization that is still in awe of the Canadian Parliamentary system",
+  "Blitz the Pitz? How 'bout Mayhem in Meehan?!!",
+  "Power Street? I hardly know'er street!",
+
+  //Spring 2008
+  "an organization that is brining in the equestrian team to test the new pool for water polo",
+  "an organization who loves to scat like Ella Fitzgerald",
+  "an organization that matriculates three or four times a day and fives times a day on Sunday",
+  "neighhhh splash",
+  "defector I hardly know her",
+  "you can now do it in a gender-neutral double",
+
+  //2010-11
+  "Roll tide!",
+  "Simba, it's ALL YOUR FAULT!",
+  "An organization renowned for its outstanding use of wood.",
+  "Pumas!",
+];
