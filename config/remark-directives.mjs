7653ab38fa80ebaacf5c6b69a995a7a5a13e41b8
Jed Fox <git@jedfox.com>
2021-12-02 16:49:16 -0500

    fix tab & list

--- config/remark-directives.mjs
+++ config/remark-directives.mjs
@@ -1,6 +1,16 @@
 import { map } from "unist-util-map";
+
 export default () => (nodeTree) =>
   map(nodeTree, (node) => {
+    if (node.type === "textDirective" && node.name === "script-tab") {
+      return {
+        type: "element",
+        data: {
+          hName: "span",
+          hProperties: { className: "_69-tab" },
+        },
+      };
+    }
     if (node.type === "textDirective" && node.name === "sd") {
       return {
         ...node,
@@ -18,6 +28,13 @@ export default () => (nodeTree) =>
           hProperties: { className: "_69-direction" },
         },
       };
+    }
+    if (node.type === "containerDirective" && node.name === "script-list") {
+      return Object.assign(node, node.children[0], {
+        data: {
+          hProperties: { className: "_69-list" },
+        },
+      });
     }
     return node;
   });
