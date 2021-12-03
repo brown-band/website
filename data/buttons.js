b4d9b14f0643e12057cd98bb72801328e505e2dc
Jed Fox <git@jedfox.com>
2021-12-03 08:24:30 -0500

    diagnose + fix perf + cleanup data/buttons.js

--- data/buttons.js
+++ data/buttons.js
@@ -1,9 +1,8 @@
 module.exports = async () => {
   const fs = require("fs/promises");
   const path = require("path");
-  const slugify = (await import("slugify")).default;
-  const d3 = await import("d3");
-  const { pathExists } = await import("path-exists");
+  const { default: slugify } = await import("slugify");
+  const d3 = await import("d3-array"); // not "d3" because that takes ~300ms to load; see https://github.com/d3/d3/issues/3550
   const { load: loadYaml } = require("js-yaml");
 
   const readYaml = (...args) =>
@@ -12,32 +11,35 @@ module.exports = async () => {
   const buttonsDir = path.join(path.dirname(__dirname), "buttons");
   const years = (await fs.readdir(buttonsDir)).filter((y) => y !== "unknown");
 
+  /** @type {Array<[string, { [key: string]: string }]>} */
   const rawLabels = await Promise.all(
     years.map(async (year) => [year, await readYaml(year, "labels.yml")])
   );
 
   const allButtons = await Promise.all(
-    rawLabels.flatMap(([year, labels]) =>
-      Object.entries(labels).map(async ([school, label]) => {
-        const schoolId = slugify(school).toLowerCase();
-        const buttonPath = [year, `${schoolId}.jpg`];
-        return {
-          year: year.split("-")[0],
-          school,
-          schoolId,
-          label,
-          image: (await pathExists(path.join(buttonsDir, ...buttonPath)))
-            ? "/buttons/" + buttonPath.join("/")
-            : null,
-        };
-      })
-    )
-  );
+    rawLabels.map(async ([year, labels]) => {
+      const buttonImages = await fs.readdir(path.join(buttonsDir, year));
+      return Promise.all(
+        Object.entries(labels).map(async ([school, label]) => {
+          const schoolId = slugify(school).toLowerCase();
+          return {
+            year: year.split("-")[0],
+            school,
+            schoolId,
+            label,
+            image: buttonImages.includes(`${schoolId}.jpg`)
+              ? `/buttons/${year}/${schoolId}.jpg`
+              : null,
+          };
+        })
+      );
+    })
+  ).then((arr) => arr.flat());
 
+  /** @type {{ [key: string]: { color: string, type: "ivy" | "recent" | "other", mascot?: string } }} */
   const schools = await fs
     .readFile(path.join(__dirname, "schoolColors.yml"))
     .then(loadYaml);
-  const schoolNames = Object.keys(schools);
 
   const allBySchool = d3
     .groups(allButtons, (d) => d.school)
@@ -47,6 +49,11 @@ module.exports = async () => {
       buttons: d3.reverse(
         buttons.map(({ school: _, schoolId: _2, ...button }) => button)
       ),
+      ...(schools[buttons[0].schoolId] ??
+        /** @type {const} */ ({
+          type: "other",
+          color: "black",
+        })),
     }));
 
   const currentYear = years[years.length - 1];
@@ -57,31 +64,7 @@ module.exports = async () => {
     ),
 
     schools,
-    bySchool: {
-      ivy: schoolNames
-        .filter((id) => schools[id].type === "ivy")
-        .map((id) => ({
-          ...allBySchool.find((s) => s.id === id),
-          ...schools[id],
-        })),
-      recent: schoolNames
-        .filter((id) => schools[id].type === "recent")
-        .map((id) => ({
-          ...allBySchool.find((s) => s.id === id),
-          ...schools[id],
-        })),
-      other: d3.sort(
-        allBySchool
-          .filter((s) => !schoolNames.includes(s.id))
-          .map((s) => ({ ...s, color: "black" }))
-          .concat(
-            allBySchool
-              .filter((s) => schools[s.id]?.type === "other")
-              .map((s) => ({ ...s, ...schools[s.id] }))
-          ),
-        (d) => d.name
-      ),
-    },
+    bySchool: Object.fromEntries(d3.groups(allBySchool, (d) => d.type)),
 
     unknown: (await readYaml("unknown", "labels.yml")).map(
       ({ imageName, ...button }) => ({
