2b23bf8298c521b3d6a80f0e4c73ed744a486521
Jed Fox <git@jedfox.com>
2022-01-02 17:52:34 -0500

    update handlebars helpers 🍔

--- config/templates.js
+++ config/templates.js
@@ -34,42 +34,44 @@ const allIcons = [
       path.join(path.dirname(__dirname), "assets", "icons.svg"),
       "utf-8"
     )
+    // parsing SVG with regex. shame on me.
     .matchAll(/<symbol[^>]+id="(?<name>[^"]+)"/g),
 ].map((i) => i.groups.name);
 
 /** @type {(eleventyConfig: import("@11ty/eleventy/src/UserConfig")) => void} */
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addFilter("first", (...args) => args.find((x) => x));
-  eleventyConfig.addFilter("reverse", (arr) => [...arr].reverse());
-  eleventyConfig.addFilter("defined?", (x) => x != null);
-  eleventyConfig.addFilter("not", (x) => !x);
-  eleventyConfig.addFilter("and", (a, b) => a && b);
-  eleventyConfig.addFilter("even?", (x) => x % 2 === 0);
-  eleventyConfig.addFilter("equal?", (x, y) => x === y);
-  eleventyConfig.addFilter("gt?", (a, b) => a > b);
-  eleventyConfig.addFilter("starts-with?", (a, b) => a.startsWith(b));
+  // Handlebars gives you nothing to work with in terms of in-template logic.
+  // These helper names are vaguely inspired by Racket.
 
-  // add the site title at the end of the page title
-  eleventyConfig.addFilter("page_title", function (title) {
-    return title ? `${title} | ${this.site.title}` : this.site.title;
-  });
+  // comparison
+  eleventyConfig.addHandlebarsHelper("defined?", (x) => x != null);
+  eleventyConfig.addHandlebarsHelper("even?", (x) => x % 2 === 0);
+  eleventyConfig.addHandlebarsHelper("equal?", (x, y) => x === y);
+  eleventyConfig.addHandlebarsHelper("gt?", (a, b) => a > b);
 
-  // format a date for display in a script header
-  eleventyConfig.addFilter("script_date", function (date) {
-    return formatDate(date, "UTC", "EEEE, MMMM do, y");
-  });
+  // logic
+  eleventyConfig.addHandlebarsHelper("not", (x) => !x);
+  eleventyConfig.addHandlebarsHelper("and", (a, b) => a && b);
+  eleventyConfig.addHandlebarsHelper("or", (a, b) => a || b);
+
+  // misc
+  eleventyConfig.addHandlebarsHelper("reverse", (arr) => [...arr].reverse());
+  eleventyConfig.addHandlebarsHelper("starts-with?", (a, b) => a.startsWith(b));
+  eleventyConfig.addHandlebarsHelper("format-utc", (date, format) =>
+    formatDate(date, "UTC", format)
+  );
 
   // convert a list to a format that’s human readable (commas between, “and” at the end)
   // and prevent insertion of line breaks within list elements
-  eleventyConfig.addFilter("listify", function (items) {
-    return listify((items ?? []).map((s) => s.replaceAll(" ", "\xA0")));
-  });
+  eleventyConfig.addHandlebarsHelper("listify", (items) =>
+    listify((items ?? []).map((s) => s.replaceAll(" ", "\xA0")))
+  );
 
   // find the page with the given filePathStem in the given collection
-  // used by nav.njk
-  eleventyConfig.addFilter("find_page", function (filePathStem, state) {
+  // used by nav.hbs
+  eleventyConfig.addHandlebarsHelper("find-page", (id, state) => {
     const page = state.data.root.collections.all.find(
-      (p) => p.filePathStem === "/" + filePathStem
+      (p) => p.filePathStem === "/" + id
     );
     if (page) {
       return {
@@ -77,15 +79,20 @@ module.exports = (eleventyConfig) => {
         active: state.data.root.page.url === page.url ? "active" : "",
         title: page.data.title,
       };
+    } else {
+      throw new ReferenceError(
+        `[find-page] Could not find page ${JSON.stringify(id)}`
+      );
     }
   });
 
-  eleventyConfig.addFilter("debug", function (data) {
-    return JSON.stringify(data, null, 2) || String(data);
-  });
+  eleventyConfig.addHandlebarsHelper(
+    "debug",
+    (data) => JSON.stringify(data, null, 2) || String(data)
+  );
 
-  // auto TOC
-  eleventyConfig.addFilter("auto-toc", function (toc, content) {
+  // generate the table of contents
+  eleventyConfig.addHandlebarsHelper("auto-toc", function (toc, content) {
     if (toc != null) return toc;
 
     // this is a hot path, so do a quick out if there are no headings
@@ -104,20 +111,17 @@ module.exports = (eleventyConfig) => {
   });
 
   // insert an icon
-  eleventyConfig.addHandlebarsShortcode(
-    "icon",
-    function (name, { size = 24 } = {}) {
-      if (!allIcons.includes(name)) {
-        throw new ReferenceError(`Unknown icon '${name}'`);
-      }
+  eleventyConfig.addHandlebarsHelper("icon", (name, { size = 24 } = {}) => {
+    if (!allIcons.includes(name)) {
+      throw new ReferenceError(`Unknown icon '${name}'`);
+    }
 
-      return new (require("handlebars").SafeString)(
-        /* HTML */ `
+    return new (require("handlebars").SafeString)(
+      /* HTML */ `
       <svg class="icon" width="${size}" height="${size}">
         <use href="/assets/icons.svg#${name}" />
       </svg>
     `.trim()
-      );
-    }
-  );
+    );
+  });
 };
