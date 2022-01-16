dbb63599b3aa8579bda8a2a75b3a2247263fffb1
Jed Fox <git@jedfox.com>
2022-01-16 14:33:48 -0500

    Bump Prettier to address strange jsx formatting

--- pages/leadership.jsx
+++ pages/leadership.jsx
@@ -13,37 +13,31 @@ exports.default = async ({ people }) => {
     <>
       <link rel="stylesheet" href="/assets/css/people.css" />
 
-      {
-        await (
-          <Markdown
-            content={`
+      {await (
+        <Markdown
+          content={`
               The **Band Board** is a six-member elected delegation of band members who manage both the behind-the-scenes and public day to day operations of the Band. The Band Board is elected for a one-year term that begins and ends at halftime at the last football game in November. If a position becomes vacant mid-term, that position is filled via a special election as soon as it is feasible. Specifics on election policy can be found in our bland yet informative [constitution](/constitution). Please feel free to email the appropriate Band Board member by clicking on their name. If you're not sure which Band Board member is appropriate, you may simply email the President, or all of Band Board. If your band is traveling to Brown, or if you have specific questions about band activities or events, the person to contact is the Corresponding Secretary. For general comments, suggestions, etc. please feel free to mail any member of the Band Board.
             `}
-          />
-        )
-      }
+        />
+      )}
 
       <Heading title="Band Board Officers" id="band-board" section="IIIA" />
 
       <p>For the term ending November 2022</p>
 
-      {
-        await Promise.all(
-          people.bandBoard.map((person, i) => (
-            <Person person={person} even={i % 2 === 0} />
-          ))
-        )
-      }
+      {await Promise.all(
+        people.bandBoard.map((person, i) => (
+          <Person person={person} even={i % 2 === 0} />
+        ))
+      )}
 
       <Heading title="Conductors" id="conductors" section="IIIC1" />
 
-      {
-        await Promise.all(
-          people.conductors.map((person, i) => (
-            <Person person={person} even={i % 2 === 0} />
-          ))
-        )
-      }
+      {await Promise.all(
+        people.conductors.map((person, i) => (
+          <Person person={person} even={i % 2 === 0} />
+        ))
+      )}
 
       <Heading title="Section Leaders" id="section-leaders" section="IIIB" />
 
@@ -119,23 +113,21 @@ async function Person({ even, person }) {
 async function PeopleTable({ sections }) {
   return (
     <table class="table">
-      {
-        await Promise.all(
-          sections.flatMap((section) =>
-            section.people.map(async (person, i) => (
-              <tr>
-                <th class="people-table-head">
-                  {i === 0 && (await (<Markdown content={section.name} />))}
-                </th>
-                <td>
-                  <a href={"mailto:" + person.email}>{person.name}</a> '
-                  {person.year}
-                </td>
-              </tr>
-            ))
-          )
+      {await Promise.all(
+        sections.flatMap((section) =>
+          section.people.map(async (person, i) => (
+            <tr>
+              <th class="people-table-head">
+                {i === 0 && (await (<Markdown content={section.name} />))}
+              </th>
+              <td>
+                <a href={"mailto:" + person.email}>{person.name}</a> '
+                {person.year}
+              </td>
+            </tr>
+          ))
         )
-      }
+      )}
     </table>
   );
 }
