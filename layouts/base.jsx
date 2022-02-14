dc137acefaec5cbfa226879569f8146ce9adede2
Jed Fox <git@jedfox.com>
2022-02-13 22:00:45 -0500

    Icons, manifest

--- layouts/base.jsx
+++ layouts/base.jsx
@@ -15,6 +15,35 @@ exports.default = ({ title, site, bodyAttrs, content }) => (
         <link rel="stylesheet" href="/assets/vendor/bootstrap.min.css" />
         <link rel="stylesheet" href="/assets/css/base.css" />
 
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/images/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/images/favicons/32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/images/favicons/16.png"
+        />
+        <link rel="manifest" href="/brown-band.webmanifest" />
+        <meta
+          name="theme-color"
+          content="#382710"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#483314"
+          media="(prefers-color-scheme: dark)"
+        />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
