490a6c1a4ceaa53343acc79d6c3a6e44cd2e8088
Jed Fox <git@jedfox.com>
2022-02-23 13:00:47 -0500

    reorganize scripts page

--- pages/scripts.jsx
+++ pages/scripts.jsx
@@ -5,49 +5,86 @@ exports.data = {
     "The Brown Band shows possess a unique satirical humor that only could come from the twisted minds of our members. Although many hours of brainstorming go into each script, the true talent behind our show is our scriptwriters. Pay attention to the names that appear on the season’s page! So if you are ready, choose a season below.",
 };
 
-exports.default = ({ scripts }) => (
-  <>
-    <p class="alert alert-warning" role="alert">
-      <strong>Please note</strong>: The Brown Band has been writing scripts for
-      over 60 years. Over this time, cultural norms have changed significantly.
-      We recognize that some of the humor in our scripts goes beyond “edgy” and
-      is at times discriminatory or otherwise highly offensive. We’ve decided to
-      leave the scripts unchanged — typos included — as a part of an honest
-      historical record. Don’t let this scare you away, though. Check out our
-      more recent scripts and consider becoming a scriptwriter yourself!
-    </p>
+exports.default = async ({ scripts }) => {
+  const { groups, reverse } = await import("d3-array");
 
-    <table
-      class="table table-striped table-sm text-center"
-      style="width: 250px"
-    >
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th colspan="2">Scripts</th>
-        </tr>
-      </thead>
-      <tbody>
-        {[...scripts.years].reverse().map((year) => (
-          <tr>
-            <td class="py-0 align-middle">{year.year}</td>
-            <td class="fw-bold py-0">
-              {year.fall && (
-                <a class="btn btn-link fw-bold" href={"/" + year.fall}>
-                  Fall
-                </a>
-              )}
-            </td>
-            <td class="fw-bold py-0">
-              {year.spring && (
-                <a class="btn btn-link fw-bold" href={"/" + year.spring}>
-                  Spring
-                </a>
-              )}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </>
-);
+  const button = "btn btn-link fw-bold px-2 py-1 text-dark";
+
+  const thisYear = scripts.years[scripts.years.length - 1];
+
+  const fallSpring = (year) => [
+    year.fall ? (
+      <a class={button} href={"/" + year.fall}>
+        Fall
+      </a>
+    ) : (
+      <button class={button} disabled style="opacity: 0.4">
+        Fall
+      </button>
+    ),
+    year.spring ? (
+      <a class={button} href={"/" + year.spring}>
+        Spring
+      </a>
+    ) : (
+      <button class={button} disabled style="opacity: 0.4">
+        Spring
+      </button>
+    ),
+  ];
+
+  return (
+    <>
+      <h3>Check out this year’s scripts!</h3>
+      {thisYear.fall && (
+        <a
+          class="btn btn-link btn-lg px-0 text-decoration-none"
+          href={"/" + thisYear.fall}
+        >
+          <u>Fall</u> →
+        </a>
+      )}
+      {thisYear.spring && (
+        <p>
+          <a
+            class="btn btn-link btn-lg px-0 text-decoration-none"
+            href={"/" + thisYear.spring}
+          >
+            <u>Spring</u> →
+          </a>
+        </p>
+      )}
+
+      <h2>Script Archive</h2>
+
+      <p class="alert alert-warning mt-2" role="alert">
+        <strong>Please note</strong>: The Brown Band has been writing scripts
+        for over 60 years. Over this time, cultural norms have changed
+        significantly. We recognize that some of the humor in our scripts goes
+        beyond “edgy” and is at times discriminatory or otherwise highly
+        offensive. We’ve decided to leave the scripts unchanged — typos included
+        — as a part of an honest historical record. Don’t let this scare you
+        away, though. Check out our more recent scripts and consider becoming a
+        scriptwriter yourself!
+      </p>
+
+      <dl style="columns: 240px">
+        {groups(reverse(scripts.years).slice(1), (d) => d.year.slice(0, 3)).map(
+          ([decade, years], i) => (
+            <div class={i > 0 && "mt-3"} style="break-inside: avoid-column">
+              <dt>{decade}0s</dt>
+              {reverse(years).map((year) => (
+                <dd class="d-flex align-items-center ms-3 mb-0">
+                  <span style="font-family: var(--bs-font-sans-serif); font-variant: tabular-nums">
+                    {year.year}
+                  </span>
+                  : {fallSpring(year)}
+                </dd>
+              ))}
+            </div>
+          )
+        )}
+      </dl>
+    </>
+  );
+};
