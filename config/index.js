bce2b8668ac51654eccb20f538131cf8a02a5637
Jed Fox <git@jedfox.com>
2022-09-05 14:49:51 -0400

    Update 11ty

--- config/index.js
+++ config/index.js
@@ -24,6 +24,7 @@ module.exports = (eleventyConfig) => {
    */
   eleventyConfig.setLibrary("md", {
     set: () => {},
+    disable: () => {},
     render: (str) => import("./markdown.mjs").then(({ render }) => render(str)),
   });
 
